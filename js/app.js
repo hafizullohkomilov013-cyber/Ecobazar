382ddd5b1feeb24e9f4c68cc136f0f7d05672e4b
hafizullohkomilov013-cyber <hafizullohkomilov013@gmail.com>
2025-11-27 20:48:59 +0500

    first commit

---
+++ js/app.js
@@ -0,0 +1,23 @@
+
+
+
+
+
+function updateTime () {
+    let now = new Date ()
+    
+    let date = now.getDate()
+    let hour = now.getHours()
+    let min = now.getMinutes()
+    let sec = now.getSeconds()
+
+    if(date < 10) date = "0" + date
+    if(hour < 10) hour = "0" + hour
+    if(min < 10) min = "0" + min
+    if(sec< 10) sec = "0" +sec
+
+    document.getElementById('clock').textContent = `${date}  : ${hour} : ${min} : ${sec}`
+}
+updateTime()
+
+setInterval(updateTime, 1000)
