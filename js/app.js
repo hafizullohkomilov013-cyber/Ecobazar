caa2dad245f9669098db93a9012209ede9623868
hafizullohkomilov013-cyber <hafizullohkomilov013@gmail.com>
2025-11-29 22:04:06 +0500

    tugadi

--- js/app.js
+++ js/app.js
@@ -23,3 +23,79 @@ updateTime()
 setInterval(updateTime, 1000)
 
 
+
+    // const countEl = document.querySelectorAll(".count-product1");
+    // const countEl1 = document.getElementsByClassName("count")[0];
+
+    // let count = Number(countEl[0].textContent);
+
+    // function updateAll() {
+    // countEl.forEach((el) => (el.textContent = count));
+    // countEl1.textContent = count;
+    // }
+    // console.log(count);
+
+
+    // function plus() {
+    // count++;
+    // updateAll();
+    // }
+
+    // function minus() {
+    // if (count <= 0) return;
+    // count--;
+    // updateAll();
+    // }
+    // const countEl2 = document.querySelectorAll(".count-product2");
+
+
+    // function updateAll() {
+    // countEl2.forEach((el) => (el.textContent = count));
+    // countEl1.textContent = count;
+    // }
+    // console.log(count);
+
+
+    // function plus2() {
+    // count++;
+    // updateAll();
+    // }
+
+    // function minus2() {
+    // if (count <= 0) return;
+    // count--;
+    // updateAll();
+    // }
+
+
+
+
+let counts = Array(16).fill(0);
+
+const totalEl = document.querySelector(".count2");
+const products = document.querySelectorAll(".count-product");
+
+function updateTotal() {
+  const total = counts.reduce((a, b) => a + b, 0);
+  totalEl.textContent = total;
+}
+
+function plus(id) {
+  counts[id - 1]++; 
+  updateElement(id);
+  updateTotal();
+}
+
+function minus(id) {
+  if (counts[id - 1] <= 0) return;
+  counts[id - 1]--;
+  updateElement(id);
+  updateTotal();
+}
+
+function updateElement(id) {
+  const el = document.querySelector(`.count-product[data-id="${id}"]`);
+  el.textContent = counts[id - 1];
+}
+
+
